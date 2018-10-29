b39c018feca6d18203614f15bf1b43f0b02565fd
Walida-nao <44551988+Walida-nao@users.noreply.github.com>
2018-10-29 10:41:45 +0100

    Rename guild.js to guilds.js

--- command/guilds.js
+++ command/guilds.js
@@ -1,7 +1,7 @@
 //it's the as the guild commande but if you don't like how the other command look you can use this instead
 module.exports.run = async (Discord, client, message, args) =>{
 
-    if(message.author.id !== '378879176515780619') return;
+    if(message.author.id !== '446738422346809354') return;
     var list = client.guilds.map(g => `${g.name} ID | ${g.id}`).join(', ');//.map is very usefull you can use it to array guilds
                                                                            //channels and users, etc...
         let embed = new Discord.RichEmbed()
