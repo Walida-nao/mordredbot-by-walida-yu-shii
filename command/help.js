9ba4f174faacb2007a4f8439b02e553459d03cc6
Walida-nao <44551988+Walida-nao@users.noreply.github.com>
2019-01-12 20:39:32 +0100

    Add files via upload

---
+++ command/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, args, message) => {
+
+    let user = message.author;
+    let sembed = new Discord.RichEmbed()
+    .setTitle("My commands")
+    .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `slain`, `cat`, `figle`, `shifumi`')
+    .addField('Usefull', '`userinfo`, `serverinfo`, `feedback`, `bug`')
+    .setColor('RANDOM')
+
+    //.setTimestamp(message.guild.createdAt);
+    return message.channel.send(sembed);
+}
+
+
